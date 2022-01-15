bac9068c3e0918c468bc3131563442f4425cd97b
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-01-15 21:39:10 +0900

    I just started my portfolio by react and tailwinds.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-600 text-3xl font-bold underline'>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
