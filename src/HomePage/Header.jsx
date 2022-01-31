df2504d40c4e578d6e17ef1409077e9d80ec9f1d
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-01-31 16:02:40 +0900

    I fixed the animation size problem by using w-fit in the css tailwinds so the overflow scroll is no longer visible.

--- src/HomePage/Header.jsx
+++ src/HomePage/Header.jsx
@@ -5,7 +5,7 @@ import '../index.css';
 function Header() {
   return (
   <div id="background-animation" className='bg-lightblue h-48'>
-      <p id="name-animation" className='sm:flex wrap relative text-lightblue text-3xl sm:text-5xl'>Stephen Dilks</p>
+      <p id="name-animation" className='sm:flex w-fit wrap relative text-lightblue text-3xl sm:text-5xl'>Stephen Dilks</p>
       <p id="text-animation" className='allura text-lightblue text-center relative text-2xl sm:text-4xl'>Front End Web Developer in Japan!</p>
   </div>
   )
